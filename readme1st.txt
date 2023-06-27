309728b3aa4b98c21c400c67aa6326fbbc561a81
Anton Zotov <>
2023-06-27 13:00:40 -0700

    first commit

---
+++ readme1st.txt
@@ -0,0 +1,5 @@
+R6}.n-"p!J>Qkv.
+antonzotov
+antonzotov@hotmail.com
+https://www.themoviedb.org/settings/api
+https://loading.io/ to get the loading assets
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { AiFillHome } from "react-icons/ai";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import MenuItem from "./MenuItem";
+import DarkModeSwitch from "./DarkModeSwitch";
+
+const Header: React.FC = () => (
+  <div className="flex justify-between mx-2 max-w-6xl sm:mx-auto items-center py-6">
+    <div className="flex">
+      <MenuItem title="HOME" address="/" Icon={AiFillHome} />
+      <MenuItem title="ABOUT" address="/about" Icon={BsFillInfoCircleFill} />
+    </div>
+    <div className="flex items-center space-x-5">
+      <DarkModeSwitch />
+      <Link href="/">
+        <h2 className="text-2xl">
+          <span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">
+            IMDb
+          </span>
+          <span className="text-xl hidden sm:inline">Clone</span>
+        </h2>
+      </Link>
+    </div>
+  </div>
+);
+
+export default Header;
